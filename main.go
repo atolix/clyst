1d9ed735df0ebec5b4a9a080c261501a714fa1ee
atolix <82761106+atolix@users.noreply.github.com>
2025-09-14 16:46:37 +0900

    selectable

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
 	"gopkg.in/yaml.v3"
 )
 
@@ -13,6 +17,39 @@ type OpenApiSpec struct {
 
 type Operation struct {
 	Summary string `yaml:"summary"`
+}
+
+type endpointItem struct {
+	title, desc string
+}
+
+func (i endpointItem) Title() string       { return i.title }
+func (i endpointItem) Description() string { return i.desc }
+func (i endpointItem) FilterValue() string { return i.title }
+
+type model struct {
+	list list.Model
+}
+
+func newModel(items []list.Item) model {
+	const defaultWidth = 50
+	l := list.New(items, list.NewDefaultDelegate(), defaultWidth, 40)
+	l.Title = "Api Endpoints"
+	return model{list: l}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return lipgloss.NewStyle().Margin(1, 2).Render(m.list.View())
 }
 
 func main() {
@@ -26,9 +63,22 @@ func main() {
 		panic(err)
 	}
 
+	var items []list.Item
 	for path, methods := range spec.Paths {
 		for method, op := range methods {
-			fmt.Printf("%s %s - %s\n", method, path, op.Summary)
+			items = append(items, endpointItem{
+				title: fmt.Sprintf("%s %s", method, path),
+				desc:  op.Summary,
+			})
 		}
 	}
+
+	m := newModel(items)
+	_, err = tea.NewProgram(m).Run()
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("bye")
 }
