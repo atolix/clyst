7757ca43227ceea874b6ff23023c6046eca69bc1
atolix <82761106+atolix@users.noreply.github.com>
2025-09-14 16:03:32 +0900

    check example

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	count int
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "up":
+			m.count++
+		case "down":
+			m.count--
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	return fmt.Sprintf("Count: %d\n[↑] +1  [↓] -1  [q] quit\n", m.count)
+}
 
 func main() {
-	fmt.Println("It works.")
+	p := tea.NewProgram(model{})
+	if err := p.Start(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
