94092852045013c325a44d39c3661817bd583d65
atolix <82761106+atolix@users.noreply.github.com>
2025-09-15 13:54:05 +0900

    box split

--- tui/tui.go
+++ tui/tui.go
@@ -26,6 +26,9 @@ func (i EndpointItem) FilterValue() string { return i.Path }
 type Model struct {
 	list     list.Model
 	Selected *EndpointItem
+	result   string
+	width    int
+	height   int
 }
 
 func NewStyleDelegate() list.DefaultDelegate {
@@ -71,6 +74,10 @@ func (m Model) Init() tea.Cmd {
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "enter":
@@ -87,25 +94,31 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	left := m.list.View()
+	half := m.width / 2
 
-	var right string
+	return lipgloss.JoinHorizontal(lipgloss.Top, ListView(m, half), DetailBox(m, half))
+}
+
+func ListView(m Model, width int) string {
+	return lipgloss.NewStyle().Width(width).Border(lipgloss.NormalBorder()).Render(m.list.View())
+}
+
+func DetailBox(m Model, width int) string {
+	var detail string
 	if i, ok := m.list.SelectedItem().(EndpointItem); ok {
 		parsed, err := json.MarshalIndent(i.Operation, "", "  ")
 		if err == nil {
 			var buf bytes.Buffer
 			quick.Highlight(&buf, string(parsed), "json", "terminal", "github")
-			right = buf.String()
+			detail = buf.String()
 		} else {
-			right = "error formatting JSON"
+			detail = "error formatting JSON"
 		}
 	} else {
-		right = "No item selected"
+		detail = "No item selected"
 	}
 
-	rightBox := lipgloss.NewStyle().Width(100).Padding(1, 2).Border(lipgloss.RoundedBorder()).Render(right)
-
-	return lipgloss.JoinHorizontal(lipgloss.Top, left, rightBox)
+	return lipgloss.NewStyle().Width(width).Padding(1, 2).Border(lipgloss.RoundedBorder()).Render(detail)
 }
 
 func Run(items []list.Item) (*EndpointItem, error) {
