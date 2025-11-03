d7e1e09d28547c281015177a0d3b4f1511a3e47c
atolix <82761106+atolix@users.noreply.github.com>
2025-11-03 20:12:03 +0900

    fix

--- tui/selector/endpoints.go
+++ tui/selector/endpoints.go
@@ -76,7 +76,7 @@ func (m endpointModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m endpointModel) View() string {
 	listWidth := m.width / 2
-	detailWidth := m.width / 2
+	detailWidth := m.width / 2 - 5
 	height := m.height / 2
 
 	return lipgloss.JoinHorizontal(lipgloss.Top, listView(m, listWidth, height), detailBox(m, detailWidth, height))
